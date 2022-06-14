5f4e3c9a2c47bf1bce772ff326c55b5628bfe472
Brian <brian.lockerbie@gmail.com>
2022-06-13 20:25:41 -0700

    created get all and single candidate routes

--- server.js
+++ server.js
@@ -21,25 +21,46 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
 );
 
-// GET a single candidate
-db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+// Get all candidates
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+  
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+});
+
+// Get a single candidate
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+  
+    db.query(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+  });
+
+// Delete a candidate
+db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
     if (err) {
       console.log(err);
     }
-    console.log(row);
+    console.log(result);
 });
-
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
-});
-
-// Delete a candidate
-//db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
-//    if (err) {
-//      console.log(err);
-//    }
-//    console.log(result);
-// });
 
 // Create a candidate
 const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
